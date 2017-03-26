fbba7780183df5a9f2ce8b3762be355ef87dbe97
radekkania <radekkania7@wp.pl>
2017-03-26 13:51:41 +0200

    implemented BufferQueueImpl

--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,20 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+	
+	private BlockingQueue<Order> queue = new LinkedBlockingQueue<>();
+	
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		queue.put(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		return queue.take();
 	}
 }
